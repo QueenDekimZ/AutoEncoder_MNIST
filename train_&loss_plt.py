1512870f477b727f1a7de5f754af450a92ba706e
QueenDekimZ <37538056+QueenDekimZ@users.noreply.github.com>
2019-08-30 02:05:37 +0800

    commit

---
+++ train_&loss_plt.py
@@ -0,0 +1,84 @@
+from keras.datasets import mnist
+import numpy as np
+import keras
+from keras.utils import np_utils
+from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from keras.models import Model, load_model
+import matplotlib.pyplot as plt
+
+class LossHistory(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = {'batch':[], 'epoch':[]}
+        self.accuracy = {'batch':[], 'epoch':[]}
+        self.val_loss = {'batch':[], 'epoch':[]}
+        self.val_acc = {'batch':[], 'epoch':[]}
+ 
+    def on_batch_end(self, batch, logs={}):
+        self.losses['batch'].append(logs.get('loss'))
+        self.accuracy['batch'].append(logs.get('acc'))
+        self.val_loss['batch'].append(logs.get('val_loss'))
+        self.val_acc['batch'].append(logs.get('val_acc'))
+ 
+    def on_epoch_end(self, batch, logs={}):
+        self.losses['epoch'].append(logs.get('loss'))
+        self.accuracy['epoch'].append(logs.get('acc'))
+        self.val_loss['epoch'].append(logs.get('val_loss'))
+        self.val_acc['epoch'].append(logs.get('val_acc'))
+ 
+    def loss_plot(self, loss_type):
+        iters = range(len(self.losses[loss_type]))
+        plt.figure()
+        # acc
+        plt.plot(iters, self.accuracy[loss_type], 'r', label='train acc')
+        # loss
+        plt.plot(iters, self.losses[loss_type], 'g', label='train loss')
+        if loss_type == 'epoch':
+            # val_acc
+            plt.plot(iters, self.val_acc[loss_type], 'b', label='val acc')
+            # val_loss
+            plt.plot(iters, self.val_loss[loss_type], 'k', label='val loss')
+        plt.grid(True)
+        plt.xlabel(loss_type)
+        plt.ylabel('acc-loss')
+        plt.legend(loc="upper right")
+        plt.show()
+
+
+(x_train, _), (x_test, _) = mnist.load_data()
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))  
+
+noise_factor = 0.4
+x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
+x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
+x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+
+input_img = Input(shape=(28, 28, 1,)) # N * 28 * 28 * 1
+x = Conv2D(32, (3, 3), padding='same', activation='relu')(input_img) # 28 * 28 * 32
+x = MaxPooling2D((2, 2), padding='same')(x) # 14 * 14 * 32
+x = Conv2D(32, (3, 3), padding='same', activation='relu')(x) # 14 * 14 * 32
+encoded = MaxPooling2D((2, 2), padding='same')(x) # 7 * 7 * 32
+
+# 7 * 7 * 32
+x = Conv2D(32, (3, 3), padding='same', activation='relu')(encoded) # 7 * 7 * 32
+x = UpSampling2D((2, 2))(x) # 14 * 14 * 32
+x = Conv2D(32, (3, 3), padding='same', activation='relu')(x) # 14 * 14 * 32
+x = UpSampling2D((2, 2))(x) # 28 * 28 * 32
+decoded = Conv2D(1, (3, 3), padding='same', activation='sigmoid')(x) # 28 * 28 * 1
+
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+history = LossHistory()
+autoencoder.fit(x_train_noisy, x_train,
+                epochs=66,
+                batch_size=128,
+                shuffle=True,
+                validation_data=(x_test_noisy, x_test),
+                validation_steps=None,
+                callbacks=[history])
+history.loss_plot('epoch')
+autoencoder.save('vae_mnist.h5')
